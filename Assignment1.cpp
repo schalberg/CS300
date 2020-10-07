685a30ee187c1f552f1d54681dec65622429d70f
Adam Schalberg <theschalbergs@gmail.com>
2020-10-07 13:19:17 -0700

    first commit

---
+++ Assignment1.cpp
@@ -0,0 +1,12 @@
+// Write a simple C++ phonebook application program
+// the program must: 
+//    read the contacct information from a given input file
+//    into a dynamically created array of Contact objects. 
+//    Each line of the input line includes name and phone 
+//    information of a contact. Assume that each name has a 
+//    single part
+//
+//    Allow to perform operations on array of data such as 
+//    search for a person, create a new contact, or delete 
+//    an existing contact. (Also include a "Quit")  
+
